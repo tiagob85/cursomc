eb57a7ddc20fdfe2504e967748fd9b9f14b7efd2
tiagob85 <tiago.gpes@gmail.com>
2020-08-25 16:40:25 -0300

    Pedido, estadopagamento, pagamento

--- src/main/java/com/example/cursomc/domain/Cliente.java
+++ src/main/java/com/example/cursomc/domain/Cliente.java
@@ -40,6 +40,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -108,6 +111,14 @@ public class Cliente implements Serializable{
 		this.tipo = tipo.getCod();
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}		
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -131,7 +142,9 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
+
+
 	
 	
 	
